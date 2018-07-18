55b2c65ee6d98a1b61ee2bf4587d6a3512552605
adreyfatawallah <muh.adrey@yahoo.com>
2018-07-18 15:08:55 +0700

    clean code

--- app/src/main/java/com/example/adrey/floatingiconmusic/MainActivity.java
+++ app/src/main/java/com/example/adrey/floatingiconmusic/MainActivity.java
@@ -1,15 +1,14 @@
 package com.example.adrey.floatingiconmusic;
 
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.PixelFormat;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.Settings;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.WindowManager;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,65 +38,20 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
-            return;
-
-        if(requestCode == CODE_DRAW_OVER_OTHER_APP_PERMISSION) {
-            if(Settings.canDrawOverlays(this))
+        if (requestCode == CODE_DRAW_OVER_OTHER_APP_PERMISSION) {
+            if (Settings.canDrawOverlays(this))
                 initializeView();
-            else if(Build.VERSION.SDK_INT == Build.VERSION_CODES.O) {
-                // NOTE: This is a workaround to fix the bug in Android O where the
-                // Settings.canDrawOverlays() will always return 'false'
-                if(canDrawOverlays(this))
-                    initializeView();
+            else {
+                Toast.makeText(this, "Draw over other app permission no available. Closing the application",
+                        Toast.LENGTH_SHORT).show();
+
+                finish();
             }
         }
-
-//        if (requestCode == CODE_DRAW_OVER_OTHER_APP_PERMISSION) {
-//            if (requestCode == RESULT_OK)
-//                initializeView();
-//            else {
-//                Toast.makeText(this, "Draw over other app permission no available. Closing the application",
-//                        Toast.LENGTH_SHORT).show();
-//
-//                finish();
-//            }
-//        } else
-//            super.onActivityResult(requestCode, resultCode, data);
-    }
-
-    /**
-     * Workaround for Android O
-     */
-    public static boolean canDrawOverlays(Context context) {
-        try {
-            WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-            return windowManager != null;
-
-//            if (windowManager == null)
-//                return false;
-//
-//            final View viewToAdd = new View(context);
-//            WindowManager.LayoutParams params =
-//                    new WindowManager.LayoutParams(
-//                            0,
-//                            0,
-//                            android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O ?
-//                                    WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY : WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
-//                            WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-//                            PixelFormat.TRANSPARENT);
-//            viewToAdd.setLayoutParams(params);
-//            windowManager.addView(viewToAdd, params);
-//            windowManager.removeView(viewToAdd);
-//            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return false;
     }
 }
